056e8c063648bdb7e84925ef8b90e1e5aa0dbaef
Pranjal Chaurasia <pranjalchaurasia11@gmail.com>
2025-12-28 02:35:38 +0530

    first commit

---
+++ Backend/server.js
@@ -0,0 +1,13 @@
+const http = require('http');
+const express = require('express');
+const app = express();
+const server = http.createServer(app);
+const PORT = process.env.PORT || 3000;
+
+server.listen(PORT,()=>{
+    console.log(`Server is running on port ${PORT}`)
+})
+server.on('error', (err) => {
+    console.error('Server error:', err);
+    process.exit(1);
+});
