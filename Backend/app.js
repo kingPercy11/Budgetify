4aeb34deea41a9f05ba6a72c37f3645268b69271
Pranjal Chaurasia <pranjalchaurasia11@gmail.com>
2025-12-29 02:19:46 +0530

    user signup/in backend

---
+++ Backend/app.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const dotenv = require('dotenv');
+const cors = require('cors');
+const cookieParser = require('cookie-parser');
+const connectToDb = require('./db/db');
+const userRoutes = require('./routes/user.routes');
+dotenv.config();
+
+const app = express();
+
+connectToDb();
+
+// Middleware
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
+
+app.get('/', (req, res) => {
+    res.send("Hello P");
+});
+
+app.use('/users', userRoutes);
+
+module.exports = app;
---
+++ Backend/services/user.services.js
@@ -0,0 +1,14 @@
+const userModel = require('../models/user.model');
+
+module.exports.createUser = async ({username,email,password}) => {
+    if(!username || !email || !password){
+        throw new Error('All fields are required');
+    }
+    const user = await userModel.create({
+        username,
+        email,
+        password   
+    });
+    return user;
+}
+
